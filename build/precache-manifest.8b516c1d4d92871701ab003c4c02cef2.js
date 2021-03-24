6071c67471df36c8f9234c5eadd742fab00fc724
DavidDGTNTSchool <davidgamingondyt@outlook.com>
2021-03-24 19:06:31 +0000

    Added build folder

---
+++ build/precache-manifest.8b516c1d4d92871701ab003c4c02cef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e635c09aeec268933633bca9771672c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac07047ae33680d8dce",
+    "url": "/static/css/main.04cc722e.chunk.css"
+  },
+  {
+    "revision": "ba05dc0f6fa4e0db260b",
+    "url": "/static/js/2.284ed8c8.chunk.js"
+  },
+  {
+    "revision": "9ac07047ae33680d8dce",
+    "url": "/static/js/main.a4ac1eda.chunk.js"
+  },
+  {
+    "revision": "2ca04ddeec7680690004",
+    "url": "/static/js/runtime-main.8ff9afc1.js"
+  }
+]);
